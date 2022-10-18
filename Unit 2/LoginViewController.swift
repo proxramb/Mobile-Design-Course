93ee17c2628c8556051ae3cb744521350a04c516
proxramb <82348142+proxramb@users.noreply.github.com>
2022-10-18 13:53:47 -0400

    HEHE

---
+++ Unit 2/LoginViewController.swift
@@ -0,0 +1,76 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Olivia Razzo on 10/5/22.
+//
+
+import UIKit
+import Parse
+
+
+class LoginViewController: UIViewController {
+    
+    
+    // Outlets!
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    //Buttons Actions!
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password:password) {
+          (user, error) in
+          if user != nil {
+              self.performSegue(withIdentifier: "loginSegue", sender: nil)
+          } else {
+              print ("Error: \(error?.localizedDescription)")
+          }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { (success, error) in
+            if success{
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print ("Error: \(error?.localizedDescription)")
+            }
+            
+            
+        }
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Unit 2/Pods/Target Support Files/Pods-Flixterr-FlixterrUITests/Pods-Flixterr-FlixterrUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Flixterr_FlixterrUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Flixterr_FlixterrUITests
-@end
